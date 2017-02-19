6e74cb1214f108c055a6144040447fd436d39d06
saibot777 <stefantrajkovic777@gmail.com>
2017-02-19 13:21:19 +0100

    structural dom manipulation implemented

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /**
  * Created by stefan.trajkovic on 19.2.2017..
  */
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+        }
+    }
+})
