791f8f875102d371b0b9d33271b5e7248ddfcb9a
saibot777 <stefantrajkovic777@gmail.com>
2017-02-19 14:08:58 +0100

    attack, checkWin functions done

--- app.js
+++ app.js
@@ -13,6 +13,45 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+        },
+        attack: function() {
+            this.monsterHealth -= this.calculateDamage(3, 10);
+            if (this.checkWin()) {
+                return;
+            }
+            
+            this.playerHealth -= this.calculateDamage(5, 12);
+            this.checkWin();
+        },
+        specialAttack: function() {
+
+        },
+        heal: function() {
+
+        },
+        giveUp: function() {
+
+        },
+        calculateDamage: function (min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin: function () {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You Won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You Lost! Try Again?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 })
